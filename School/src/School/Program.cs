fe405aad919ba1f8aa2b9cf92ded25002fa4b11c
Laurie Bainter <lbainter28@gmail.com>
2017-03-21 16:46:28 -0500

    Added short console program to add new student with grades and print out their total credits and GPA.

--- School/src/School/Program.cs
+++ School/src/School/Program.cs
@@ -9,6 +9,15 @@ namespace School
     {
         public static void Main(string[] args)
         {
+            Student laurie = new Student("Laurie", 54);
+            laurie.AddGrade(3, 4);
+            laurie.AddGrade(4, 3);
+            Console.WriteLine(
+                "Credits: {0}, GPA: {1}",
+                laurie.NumberOfCredits,
+                laurie.Gpa
+            );
+            Console.ReadLine();
         }
     }
 }
