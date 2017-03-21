4e9be1ac888226a934148e8c31910fcea1acd5d7
Laurie Bainter <lbainter28@gmail.com>
2017-03-21 16:39:51 -0500

    Completed Class 6 Exercises - Added constructors, Equals, and ToString; AddGrade and GetGradeLevel methods to Student class; new Grade and GradeList classes

---
+++ School/src/School/Course.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School
+{
+    public class Course
+    {
+        private string title;
+        private int courseNum;
+        private int numberOfCredits;
+        private List<Student> courseRoster;
+
+        public Course(string title, int courseNum, int numberofCredits)
+        {
+            Title = title;
+            CourseNum = courseNum;
+            NumberOfCredits = numberOfCredits;
+        }
+
+        public string Title
+        {
+            get { return title; }
+            private set { title = value; }
+        }
+
+        public int CourseNum
+        {
+            get { return courseNum; }
+            private set { courseNum = value; }
+        }
+
+        public int NumberOfCredits
+        {
+            get { return numberOfCredits; }
+            private set { numberOfCredits = value; }
+        }
+
+        public Student[] CourseRoster
+        {
+            get
+            {
+                return courseRoster.ToArray();
+            }
+            private set
+            {
+                courseRoster = new List<Student>();
+            }
+        }
+
+        public void AddStudent(Student name)
+        {
+            courseRoster.Add(name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Course other = (Course)obj;
+            return other.CourseNum == CourseNum;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("[{0}] {1}", CourseNum, Title);
+        }
+    }
+}
---
+++ School/src/School/GradeList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace School
+{
+    public class GradeList : List<Grade>
+    {
+        public int Credits
+        {
+            get
+            {
+                int totalCredits = 0;
+                foreach (Grade grade in this)
+                {
+                    totalCredits += grade.Credits;
+                }
+                return totalCredits;
+            }
+        }
+
+        public double QualityScore
+        {
+            get
+            {
+                double totalQualityScore = 0;
+                foreach (Grade grade in this)
+                {
+                    totalQualityScore += grade.QualityScore;
+                }
+                return totalQualityScore;
+            }
+        }
+
+        public double Gpa
+        {
+            get { return QualityScore / Credits; }
+        }
+    }
+}
