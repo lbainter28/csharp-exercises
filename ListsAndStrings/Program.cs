85de95dc7ef33eca6a25d5ec2d41c713909f1ae5
Laurie Bainter <lbainter28@gmail.com>
2017-04-18 00:58:26 -0500

    Class 2 Prep Exercises - ListsAndStrings exercise completed

---
+++ ListsAndStrings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListsAndStrings
+{
+    public class ListsAndStrings
+    {
+        public static void Main(string[] args)
+        {
+            List<string> strList = new List<string>() { "cat", "chinchilla", "sheep", "zebra", "horse", "giraffe", "aadvark", "jaguar" };
+            List<string> fiveCharWords = GetWords(strList);
+            for (int i = 0; i < fiveCharWords.Count; i++)
+            {
+                Console.WriteLine(fiveCharWords[i]);
+            }
+            Console.ReadLine();
+        }
+
+        public static List<string> GetWords(List<string> strList)
+        {
+            List<string> fiveCharList = new List<string>();
+
+            foreach (string str in strList)
+            {
+                if (str.ToCharArray().Count() == 5)
+                {
+                    fiveCharList.Add(str);
+                }
+            }
+            return fiveCharList;
+        }
+    }
+}
