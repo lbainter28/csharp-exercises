eba35a2b1a1f4e0ae9fb0ba4c5330ecf8cb7fa55
Laurie Bainter <lbainter28@gmail.com>
2017-04-17 20:37:29 -0500

    HelloWorld modified to prompt user for name, added name to greeting

---
+++ InputOutput/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InputOutput
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input;
+
+            Console.WriteLine("Tell us your name:");
+            input = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + input + "!");
+            Console.ReadLine();
+        }
+    }
+}
