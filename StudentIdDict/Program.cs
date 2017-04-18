d7b23bfa1ad43d8f92229059fcc5df9567168f4b
Laurie Bainter <lbainter28@gmail.com>
2017-04-18 02:29:35 -0500

    Class 2 Prep Exercises - StudentIdDict exercise completed

---
+++ StudentIdDict/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StudentIDDict
+{
+    public class StudentIDDict
+    {
+        public static void Main(string[] args)
+        {
+            Dictionary<int, string> students = new Dictionary<int, string>();
+            int newStudentId;
+            string newStudentIdStr;
+
+            Console.WriteLine("Enter your student's ID (or ENTER to finish):");
+            do
+            {
+                Console.Write("ID: ");
+                newStudentIdStr = Console.ReadLine();
+                if (newStudentIdStr != "")
+                {
+                    // Get the student's name
+                    Console.Write("name: ");
+                    string newStudentName = Console.ReadLine();
+
+                    newStudentId = int.Parse(newStudentIdStr);
+                    students.Add(newStudentId, newStudentName);
+                }
+            } while (newStudentIdStr != "");
+
+            // Print class roster
+            Console.WriteLine("\nClass roster:");
+            StringBuilder classRoster = new StringBuilder();
+
+            foreach (KeyValuePair<int, string> student in students)
+            {
+                classRoster.Append(student.Key);
+                classRoster.Append(": ");
+                classRoster.Append(student.Value);
+                classRoster.Append("\n");
+            }
+            Console.WriteLine(classRoster);
+            Console.ReadLine();
+        }
+    }
+}
