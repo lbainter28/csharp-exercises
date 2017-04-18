136ec19101b2629d5d30b76aa412e5915b34dc18
Laurie Bainter <lbainter28@gmail.com>
2017-04-18 00:35:31 -0500

    Class 2 Prep Exercises - SumListValues exercise completed

---
+++ SumListValues/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SumListValues
+{
+    public class SumListValues
+    {
+        public static void Main(string[] args)
+        {
+            List<int> numList = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Console.WriteLine(SumListValues.GetSum(numList));
+            Console.ReadLine();
+        }
+
+        public static string GetSum(List<int> numList)
+        {
+            int numSum = 0;
+            foreach (int num in numList)
+            {
+                if (num % 2 == 0)
+                    numSum += num;
+            }
+            return numSum.ToString();
+        }
+    }
+}
