71646ea400fc42d3b791b886418550411f66f76f
Laurie Bainter <lbainter28@gmail.com>
2017-04-17 21:07:18 -0500

    Class 1 Prep Exercises - Numeric Types exercise completed

---
+++ NumericTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NumericTypes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input;
+            int length;
+            int width;
+            int area;
+
+            Console.WriteLine("What is the rectangle's length?");
+            input = Console.ReadLine();
+            length = int.Parse(input);
+
+            Console.WriteLine("What is the rectangle's width?");
+            input = Console.ReadLine();
+            width = int.Parse(input);
+
+            area = length * width;
+            Console.WriteLine("The rectangle's area is: " + area.ToString());
+            Console.ReadLine();
+        }
+    }
+}
