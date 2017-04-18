0ce6accfcfdf8260b087b6a2eb2539d0c1f00ae6
Laurie Bainter <lbainter28@gmail.com>
2017-04-17 21:35:00 -0500

    Class 1 Prep Exercises - Numeric Types 2 exercise completed

---
+++ NumericTypes2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NumericTypes2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string input;
+            int milesDriven;
+            int gasGallons;
+            float milesPerGallon;
+
+            Console.WriteLine("How many miles have you driven?");
+            input = Console.ReadLine();
+            milesDriven = int.Parse(input);
+
+            Console.WriteLine("How many gallons of gas have you used?");
+            input = Console.ReadLine();
+            gasGallons = int.Parse(input);
+
+            milesPerGallon = milesDriven / gasGallons;
+            Console.WriteLine("Your MPG is: " + milesPerGallon.ToString());
+            Console.ReadLine();
+        }
+    }
+}
