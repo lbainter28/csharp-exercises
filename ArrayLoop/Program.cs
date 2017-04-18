c37964ee465b57eb30151425a80fd057e75dcd6d
Laurie Bainter <lbainter28@gmail.com>
2017-04-18 01:22:10 -0500

    Class 2 Prep Exercises - ArrayLoop exercise completed

---
+++ ArrayLoop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArrayLoop
+{
+    public class ArrayLoop
+    {
+        public static void Main(string[] args)
+        {
+            int[] intArray = { 1, 1, 2, 3, 5, 8 };
+
+            foreach (int i in intArray)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
