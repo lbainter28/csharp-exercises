81f1cec1681cf2288d73d361842a9beb5949aa57
Laurie Bainter <lbainter28@gmail.com>
2017-04-19 20:05:51 -0500

    Intro to ASP.NET Core MVC: Controllers version completed

---
+++ HelloMVC/src/HelloMVC/Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloMVC.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            //return Content("Hello World!");
+            //return Content("<h1>Hello World!</h1>");
+            return Content("<h1>Hello World!</h1>", "text/html");
+        }
+
+        public IActionResult Goodbye()
+        {
+            return Content("Goodbye");
+        }
+    }
+}
